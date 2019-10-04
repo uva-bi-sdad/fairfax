059c92617826d8a3551aa2d2e22cecc98e6d826c
Teja Pristavec <tp2sk@virginia.edu>
2019-10-04 16:00:10 +0000

    First pass at OZ mapping

---
+++ src/exploratory/oz_neighbors.R
@@ -0,0 +1,76 @@
+library(dplyr)
+library(sf)
+library(ggplot2)
+library(ggthemes)
+library(ggrepel)
+library(scales)
+
+
+#
+# Prepare data ------------------------------------------------------------------------------------
+#
+
+# Read in
+acs1317 <- st_read("./data/working/acs1317/acs1317_tract_calc.shp")
+
+# Identify zone neighbors
+zones <- acs1317 %>% filter(opprtnt == 1)
+
+neighbors <- st_touches(zones, acs1317, sparse = TRUE)
+neighborlist <- unlist(neighbors)
+
+# Test plot
+plot(st_geometry(acs1317), col = "white")
+plot(st_geometry(acs1317[neighborlist, ]), add = TRUE, col = "blue")
+plot(st_geometry(zones), col = "red", add = TRUE)
+
+# Create neighbor indicator
+acs1317 <- acs1317 %>% mutate(id = row_number(),
+                              neighbor = ifelse(id %in% neighborlist, 1, 0))
+table(acs1317$neighbor)
+
+# Test plot again with all indicators in same dataset
+plot(st_geometry(acs1317), col = "white")
+plot(st_geometry(acs1317[acs1317$neighbor == 1, ]), add = TRUE, col = "blue")
+plot(st_geometry(acs1317[acs1317$opprtnt == 1, ]), add = TRUE, col = "red")
+
+# Convert to factor
+acs1317$neighbor <- as.factor(acs1317$neighbor)
+acs1317$opprtnt <- as.factor(acs1317$opprtnt)
+
+
+#
+# Pretty plot ------------------------------------------------------------------------------------
+#
+
+# Get viridis colors
+show_col(viridis_pal()(20))
+
+# Get labels: select tracts, get points (ignore warnings), retrieve coords
+labelzones <- acs1317[acs1317$opprtnt == 1, ]
+labelneighbors <- acs1317[acs1317$neighbor == 1, ]
+
+point_zones <- st_point_on_surface(labelzones)
+point_neighbors <- st_point_on_surface(labelneighbors)
+
+coords_zone <- as.data.frame(st_coordinates(point_zones))
+coords_zone$name <- point_zones$NAME_x
+
+coords_neighbor <- as.data.frame(st_coordinates(point_neighbors))
+coords_neighbor$name <- point_neighbors$NAME_x
+
+ggplot() +
+  geom_sf(data = nc3, aes(fill = AREA)) +
+  geom_text(data = nc3_coords, aes(X, Y, label = NAME), colour = "white")
+
+# Plot
+ggplot() +
+  geom_sf(data = acs1317, size = 0.2) +
+  geom_sf(data = acs1317[acs1317$neighbor == 1, ], fill = "#440154") +
+  geom_sf(data = acs1317[acs1317$opprtnt == 1, ], fill = "#FDE725")  +
+  geom_label_repel(data = coords_zone, aes(X, Y, label = name), colour = "black", nudge_x = 3, segment.size = 0.5) + 
+  labs(title = "Fairfax County Opportunity Zones and Neighboring Tracts") +
+  theme_map() +
+  theme(plot.title = element_text(size = 16, face = "bold"))
+
+
