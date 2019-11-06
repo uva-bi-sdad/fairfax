554a13f9196a2b50a6f6006ba7dc8518d42e2890
Teja Pristavec <tp2sk@virginia.edu>
2019-11-06 13:28:28 +0000

    Added atlas import

---
+++ src/atlas/01-read.R
@@ -0,0 +1,37 @@
+library(readr)
+library(dplyr)
+
+
+#
+# Get data & filter ----------------------------------------------------------------------------------------------------
+#
+
+# Import (note outcomes will take a while)
+hhinc_jail <- read_csv("./data/original/atlas/hhinc_jail.csv", progress = show_progress())
+neighb_chars <- read_csv("./data/original/atlas/neighb_chars.csv", progress = show_progress())
+outcomes <- read_csv("./data/original/atlas/outcomes.csv", progress = show_progress())
+
+# Filter to Fairfax County and Fairfax City
+# Files contain 2010 county FIPS codes. Reference:
+# https://www.census.gov/geographies/reference-maps/2010/geo/county-wallmaps-2010.html
+# https://www2.census.gov/geo/maps/general_ref/us_base/stco2010/USstcou2010_wallmap.pdf
+# Fairfax = 059 county code
+# Fairfax City = 600 "county" code
+
+hhinc_jail_ffx <- hhinc_jail %>% filter(state == 51 & (county == 059 | county == 600))
+neighb_chars_ffx <- neighb_chars %>% filter(state == 51 & (county == 059 | county == 600))
+outcomes_ffx <- outcomes %>% filter(state == 51 & (county == 059 | county == 600))
+
+# Clean up
+remove(hhinc_jail)
+remove(neighb_chars)
+remove(outcomes)
+
+
+#
+# Write out ----------------------------------------------------------------------------------------------------
+#
+
+write_csv(hhinc_jail_ffx, "./data/working/atlas/hhinc_jail_ffx.csv", col_names = TRUE, append = FALSE)
+write_csv(neighb_chars_ffx, "./data/working/atlas/neighb_chars_ffx.csv", col_names = TRUE, append = FALSE)
+write_csv(outcomes_ffx, "./data/working/atlas/outcomes_ffx.csv", col_names = TRUE, append = FALSE)
