86293c55952208e4c9feac1265c0ac9f6e95e0ce
Teja Pristavec <tp2sk@virginia.edu>
2020-01-09 13:55:18 +0000

    Updated age

--- src/overtime/06_nhgis_age.R
+++ src/overtime/06_nhgis_age.R
@@ -31,32 +31,36 @@ fairfax <- fairfax %>% transmute(
   total2000 = AV0AA2000,
   total2010 = AV0AA2010,
   total200812 = AV0AA125,
-  # Median age
-  medage1980 = AR9AA1980,
-  medage1990 = AR9AA1990,
-  medage2000 = AR9AA2000,
-  medage2010 = AR9AA2010,
   # Proportion 65+
   propold1970 = (B57AP1970 + B57AQ1970 + B57AR1970) / total1970,
   propold1980 = (B57AP1980 + B57AQ1970 + B57AR1980) / total1980,
   propold1990 = (B57AP1990 + B57AQ1970 + B57AR1990) / total1990,
   propold2000 = (B57AP2000 + B57AQ2000 + B57AR2000) / total2000,
   propold2010 = (B57AP2010 + B57AQ2010 + B57AR2010) / total2010,
-  # Proportion <18
-  propyoung1970 = (B57AA1970 + B57AB1970 + B57AC1970 + B57AD1970) / total1970,
-  propyoung1980 = (B57AA1980 + B57AB1980 + B57AC1980 + B57AD1980) / total1980,
-  propyoung1990 = (B57AA1990 + B57AB1990 + B57AC1990 + B57AD1990) / total1990,
-  propyoung2000 = (B57AA2000 + B57AB2000 + B57AC2000 + B57AD2000) / total2000,
-  propyoung2010 = (B57AA2010 + B57AB2010 + B57AC2010 + B57AD2010) / total2010,
+  # Proportion 0-24
+  propyoung1970 = (B57AA1970 + B57AB1970 + B57AC1970 + B57AD1970 + B57AE1970 + B57AF1970 + B57AG1970 + B57AH1970) / total1970,
+  propyoung1980 = (B57AA1980 + B57AB1980 + B57AC1980 + B57AD1980 + B57AE1980 + B57AF1980 + B57AG1980 + B57AH1980) / total1980,
+  propyoung1990 = (B57AA1990 + B57AB1990 + B57AC1990 + B57AD1990 + B57AE1990 + B57AF1990 + B57AG1990 + B57AH1990) / total1990,
+  propyoung2000 = (B57AA2000 + B57AB2000 + B57AC2000 + B57AD2000 + B57AE2000 + B57AF2000 + B57AG2000 + B57AH2000) / total2000,
+  propyoung2010 = (B57AA2010 + B57AB2010 + B57AC2010 + B57AD2010 + B57AE2010 + B57AF2010 + B57AG2010 + B57AH2010) / total2010,
+  # Proportion working age (25-64)
+  propwork1970 = (B57AI1970 + B57AJ1970 + B57AK1970 + B57AL1970 + B57AM1970 + B57AN1970 + B57AO1970) / total1970,
+  propwork1980 = (B57AI1980 + B57AJ1980 + B57AK1980 + B57AL1980 + B57AM1980 + B57AN1980 + B57AO1980) / total1980,
+  propwork1990 = (B57AI1990 + B57AJ1990 + B57AK1990 + B57AL1990 + B57AM1990 + B57AN1990 + B57AO1990) / total1990,
+  propwork2000 = (B57AI2000 + B57AJ2000 + B57AK2000 + B57AL2000 + B57AM2000 + B57AN2000 + B57AO2000) / total2000,
+  propwork2010 = (B57AI2010 + B57AJ2010 + B57AK2010 + B57AL2010 + B57AM2010 + B57AN2010 + B57AO2010) / total2010
 )
 
 
 # Add 2015 data
 vars1317 <- c(
   "B01003_001", # total population
-  "B01002_002", # median age
-  "B01001_001", "B01001_003", "B01001_004", "B01001_005", "B01001_006", "B01001_027", "B01001_028", "B01001_029", "B01001_030", # less than 18
-  "B01001_020", "B01001_021", "B01001_022", "B01001_023", "B01001_024", "B01001_025", "B01001_044", "B01001_045", "B01001_046", "B01001_047", "B01001_048", "B01001_049" # older than 65
+  "B01001_001", # total for age
+  "B01001_003", "B01001_004", "B01001_005", "B01001_006", "B01001_007", "B01001_008", "B01001_009", "B01001_010", # 0-24 male
+  "B01001_027", "B01001_028", "B01001_029", "B01001_030", "B01001_031", "B01001_032", "B01001_033", "B01001_034",  # 0-24 female
+  "B01001_020", "B01001_021", "B01001_022", "B01001_023", "B01001_024", "B01001_025", "B01001_044", "B01001_045", "B01001_046", "B01001_047", "B01001_048", "B01001_049", # older than 65
+  "B01001_011", "B01001_012", "B01001_013", "B01001_014", "B01001_015", "B01001_016", "B01001_017", "B01001_018", "B01001_019",  #25-64 male
+  "B01001_035", "B01001_036", "B01001_037", "B01001_038", "B01001_039", "B01001_040", "B01001_041", "B01001_042", "B01001_043" #25-64 female
 ) 
 
 acs1317 <- get_acs(geography = "county", state = 51, county = 059, variables = vars1317, year = 2017, survey = "acs5", cache_table = TRUE, 
@@ -64,8 +68,11 @@ acs1317 <- get_acs(geography = "county", state = 51, county = 059, variables = v
 
 acs1317 <- acs1317 %>% transmute(
   total2015 = B01003_001E,
-  propyoung2015 = (B01001_003E + B01001_004E + B01001_005E + B01001_006E + B01001_027E + B01001_028E + B01001_029E + B01001_030E) / B01001_001E,
-  propold2015 = (B01001_020E + B01001_021E + B01001_022E + B01001_023E + B01001_024E + B01001_025E + B01001_044E + B01001_045E + B01001_046E + B01001_047E + B01001_048E + B01001_049E) / B01001_001E)
+  propyoung2015 = (B01001_003E + B01001_004E + B01001_005E + B01001_006E + B01001_007E + B01001_008E + B01001_009E + B01001_010E +
+                   B01001_027E + B01001_028E + B01001_029E + B01001_030E + B01001_031E + B01001_032E + B01001_033E + B01001_034E) / B01001_001E,
+  propold2015 = (B01001_020E + B01001_021E + B01001_022E + B01001_023E + B01001_024E + B01001_025E + B01001_044E + B01001_045E + B01001_046E + B01001_047E + B01001_048E + B01001_049E) / B01001_001E,
+  propwork2015 = (B01001_011E + B01001_012E + B01001_013E + B01001_014E + B01001_015E + B01001_016E + B01001_017E + B01001_018E + B01001_019E +
+                 B01001_035E + B01001_036E + B01001_037E + B01001_038E + B01001_039E + B01001_040E + B01001_041E + B01001_042E + B01001_043E) / B01001_001E)
 
 # Join
 fairfax <- as.data.frame(fairfax)
@@ -73,15 +80,15 @@ acs1317 <- as.data.frame(acs1317)
 fairfax <- base::cbind(fairfax, acs1317)
 
 # Pivot
-fairfax_lng <- fairfax %>% pivot_longer(cols = 1:23, values_to = "prop")
+fairfax_lng <- fairfax %>% pivot_longer(cols = 1:25, values_to = "prop")
 fairfax_lng <- fairfax_lng %>% arrange(name)
 fairfax_lng <- fairfax_lng %>% mutate(year = str_extract_all(name, "[0-9]{4}", simplify = TRUE),
                                       name = str_extract_all(name, "[a-zA-Z]+", simplify = TRUE))
 fairfax_lng$name <- as.factor(fairfax_lng$name)
-fairfax_lng$name <- factor(fairfax_lng$name, levels = c("medage", "propold", "propyoung", "total"),
-                           labels = c("Median age", "Proportion age 65+", "Proportion age <18", "Total population"))
+fairfax_lng$name <- factor(fairfax_lng$name, levels = c("propyoung", "propwork", "propold", "total"),
+                           labels = c("Proportion age <25", "Proportion age 25-64", "Proportion age 65+", "Total population"))
 
-fairfax_lng <- fairfax_lng %>% filter(name == "Proportion age 65+" | name == "Proportion age <18")
+fairfax_lng <- fairfax_lng %>% filter(name != "Total population")
 
 # Plot
 ggplot(fairfax_lng, aes(y = prop, x = year)) +
--- src/overtime/07_zonevsneigh.R
+++ src/overtime/07_zonevsneigh.R
@@ -3,7 +3,9 @@ library(readxl)
 library(ggplot2)
 library(ggthemes)
 library(viridis)
-
+library(tidyr)
+library(stringr)
+library(hrbrthemes)
 
 #
 # ZIP to tract --------------------------------------------------------------------------------------------------
@@ -86,10 +88,7 @@ ziptract10 <- ziptract10 %>% filter(ZIP == 22306 | ZIP == 22309 |               
 # Flag tracts in ACS data --------------------------------------------------------------------------------------------------
 #
 
-acs15 <- left_join(acs1317, ziptract15, by = c("GEOID" = "TRACT")) %>% select(GEOID, ZIP, NAME.y, NAME.x,
-                                                                              lesshs, hispanic, black, inpoverty, singleparent, 
-                                                                              onname, geometry) %>%
-                                                                       mutate(ison = ifelse(!is.na(onname), 1, 0)) %>%
+acs15 <- left_join(acs1317, ziptract15, by = c("GEOID" = "TRACT")) %>% mutate(ison = ifelse(!is.na(onname), 1, 0)) %>%
                                                                        mutate(isopp = ifelse((NAME.x == 4810 |
                                                                                               NAME.x == 4821 |
                                                                                               NAME.x == 4514 |
@@ -109,10 +108,7 @@ acs15 <- left_join(acs1317, ziptract15, by = c("GEOID" = "TRACT")) %>% select(GE
                                                                                                  NAME.x == 4216 ~ "Tract 4216",
                                                                                                  NAME.x == 4218 ~ "Tract 4218"))
 
-acs14 <- left_join(acs1216, ziptract14, by = c("GEOID" = "TRACT")) %>% select(GEOID, ZIP, NAME.y, NAME.x,
-                                                                              lesshs, hispanic, black, inpoverty, singleparent, 
-                                                                              onname, geometry) %>%
-                                                                       mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
+acs14 <- left_join(acs1216, ziptract14, by = c("GEOID" = "TRACT")) %>% mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
                                                                        mutate(isopp = ifelse((NAME.x == 4810 |
                                                                                                NAME.x == 4821 |
                                                                                                NAME.x == 4514 |
@@ -132,10 +128,7 @@ acs14 <- left_join(acs1216, ziptract14, by = c("GEOID" = "TRACT")) %>% select(GE
                                                                                                  NAME.x == 4216 ~ "Tract 4216",
                                                                                                  NAME.x == 4218 ~ "Tract 4218"))
 
-acs13 <- left_join(acs1115, ziptract13, by = c("GEOID" = "TRACT")) %>% select(GEOID, ZIP, NAME.y, NAME.x,
-                                                                              lesshs, hispanic, black, inpoverty, singleparent, 
-                                                                              onname, geometry) %>%
-                                                                       mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
+acs13 <- left_join(acs1115, ziptract13, by = c("GEOID" = "TRACT")) %>% mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
                                                                        mutate(isopp = ifelse((NAME.x == 4810 |
                                                                                                NAME.x == 4821 |
                                                                                                NAME.x == 4514 |
@@ -155,10 +148,7 @@ acs13 <- left_join(acs1115, ziptract13, by = c("GEOID" = "TRACT")) %>% select(GE
                                                                                                  NAME.x == 4216 ~ "Tract 4216",
                                                                                                  NAME.x == 4218 ~ "Tract 4218"))
 
-acs12 <- left_join(acs1014, ziptract12, by = c("GEOID" = "TRACT")) %>% select(GEOID, ZIP, NAME.y, NAME.x,
-                                                                              lesshs, hispanic, black, inpoverty, singleparent, 
-                                                                              onname, geometry) %>%
-                                                                       mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
+acs12 <- left_join(acs1014, ziptract12, by = c("GEOID" = "TRACT")) %>% mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
                                                                        mutate(isopp = ifelse((NAME.x == 4810 |
                                                                                                NAME.x == 4821 |
                                                                                                NAME.x == 4514 |
@@ -178,10 +168,7 @@ acs12 <- left_join(acs1014, ziptract12, by = c("GEOID" = "TRACT")) %>% select(GE
                                                                                                  NAME.x == 4216 ~ "Tract 4216",
                                                                                                  NAME.x == 4218 ~ "Tract 4218"))
 
-acs11 <- left_join(acs0913, ziptract11, by = c("GEOID" = "TRACT")) %>% select(GEOID, ZIP, NAME.y, NAME.x,
-                                                                              lesshs, hispanic, black, inpoverty, singleparent, 
-                                                                              onname, geometry) %>%
-                                                                       mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
+acs11 <- left_join(acs0913, ziptract11, by = c("GEOID" = "TRACT")) %>% mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
                                                                        mutate(isopp = ifelse((NAME.x == 4810 |
                                                                                                NAME.x == 4821 |
                                                                                                NAME.x == 4514 |
@@ -201,10 +188,7 @@ acs11 <- left_join(acs0913, ziptract11, by = c("GEOID" = "TRACT")) %>% select(GE
                                                                                                    NAME.x == 4216 ~ "Tract 4216",
                                                                                                    NAME.x == 4218 ~ "Tract 4218"))
 
-acs10 <- left_join(acs0812, ziptract10, by = c("GEOID" = "TRACT")) %>% select(GEOID, ZIP, NAME.y, NAME.x,
-                                                                              lesshs, hispanic, black, inpoverty, singleparent, 
-                                                                              onname, geometry) %>%
-                                                                       mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
+acs10 <- left_join(acs0812, ziptract10, by = c("GEOID" = "TRACT")) %>% mutate(ison = ifelse(!is.na(onname), 1, 0))  %>%
                                                                        mutate(isopp = ifelse((NAME.x == 4810 |
                                                                                                NAME.x == 4821 |
                                                                                                NAME.x == 4514 |
@@ -223,6 +207,10 @@ acs10 <- left_join(acs0812, ziptract10, by = c("GEOID" = "TRACT")) %>% select(GE
                                                                                                  NAME.x == 4215 ~ "Tract 4215",
                                                                                                  NAME.x == 4216 ~ "Tract 4216",
                                                                                                  NAME.x == 4218 ~ "Tract 4218"))
+
+#
+# Plot --------------------------------------------------------------------------------------------------
+#
 
 plot(st_geometry(acs15))
 plot(st_geometry(acs15[acs15$ison == 1, ]), add = TRUE, col = "blue")
@@ -279,3 +267,109 @@ ggplot() +
         legend.text = element_text(size = 11),
         legend.position = c(0.1, 0.1))  +
   scale_fill_identity(name = "Tract Type", guide = "legend", labels = c("Opportunity Neighborhood", "Opportunity Zone")) 
+
+
+#
+# Characteristics across time --------------------------------------------------------------------------------------------------
+#
+
+# Annandale
+annandale15 <- acs15 %>% filter(onname == "Annandale")
+annandale14 <- acs14 %>% filter(onname == "Annandale")
+annandale13 <- acs13 %>% filter(onname == "Annandale")
+annandale12 <- acs12 %>% filter(onname == "Annandale")
+annandale11 <- acs11 %>% filter(onname == "Annandale")
+annandale10 <- acs10 %>% filter(onname == "Annandale")
+
+ggplot(annandale15, aes(fill = hispanic)) +
+  geom_sf() +
+  theme_map()
+ggplot(annandale14, aes(fill = hispanic)) +
+  geom_sf() +
+  theme_map()
+ggplot(annandale13, aes(fill = hispanic)) +
+  geom_sf() +
+  theme_map()
+ggplot(annandale12, aes(fill = hispanic)) +
+  geom_sf() +
+  theme_map()
+ggplot(annandale11, aes(fill = hispanic)) +
+  geom_sf() +
+  theme_map()
+ggplot(annandale10, aes(fill = hispanic)) +
+  geom_sf() +
+  theme_map()
+
+annandale15 <- annandale15 %>% summarize(ntotal15 = sum(B03003_001E),
+                                         nhispanic15 = sum(B03003_003E),
+                                         nblack15 = sum(B02001_003E),
+                                         ninpov15 = sum(B17020_002E),
+                                         nlesshs15 = sum(B15003_002E + B15003_003E +  B15003_004E + B15003_005E + B15003_006E + B15003_007E + B15003_008E + B15003_009E +
+                                                         B15003_010E + B15003_011E + B15003_012E + B15003_013E + B15003_014E + B15003_015E),
+                                         nsingle15 = sum(B09005_004E + B09005_005E)) %>%
+                               mutate(prophisp15 = nhispanic15 / ntotal15,
+                                      propblack15 = nblack15 / ntotal15,
+                                      propinpov15 = ninpov15 / ntotal15,
+                                      proplesshs15 = nlesshs15 / ntotal15,
+                                      propsingle15 = nsingle15 / ntotal15) %>% select(ntotal15, prophisp15, propblack15, propinpov15, proplesshs15, propsingle15)
+
+annandale14 <- annandale14 %>% summarize(ntotal14 = sum(B03003_001E),
+                                         nhispanic14 = sum(B03003_003E),
+                                         nblack14 = sum(B02001_003E),
+                                         ninpov14 = sum(B17020_002E),
+                                         nlesshs14 = sum(B15003_002E + B15003_003E +  B15003_004E + B15003_005E + B15003_006E + B15003_007E + B15003_008E + B15003_009E +
+                                                           B15003_010E + B15003_011E + B15003_012E + B15003_013E + B15003_014E + B15003_015E),
+                                         nsingle14 = sum(B09005_004E + B09005_005E)) %>%
+                                mutate(prophisp14 = nhispanic14 / ntotal14,
+                                       propblack14 = nblack14 / ntotal14,
+                                       propinpov14 = ninpov14 / ntotal14,
+                                       proplesshs14 = nlesshs14 / ntotal14,
+                                       propsingle14 = nsingle14 / ntotal14) %>% select(ntotal14, prophisp14, propblack14, propinpov14, proplesshs14, propsingle14)
+
+annandale13 <- annandale13 %>% summarize(ntotal13 = sum(B03003_001E),
+                                         nhispanic13 = sum(B03003_003E),
+                                         nblack13 = sum(B02001_003E),
+                                         ninpov13 = sum(B17020_002E),
+                                         nlesshs13 = sum(B15003_002E + B15003_003E +  B15003_004E + B15003_005E + B15003_006E + B15003_007E + B15003_008E + B15003_009E +
+                                                           B15003_010E + B15003_011E + B15003_012E + B15003_013E + B15003_014E + B15003_015E),
+                                         nsingle13 = sum(B09005_004E + B09005_005E)) %>%
+  mutate(prophisp13 = nhispanic13 / ntotal13,
+         propblack13 = nblack13 / ntotal13,
+         propinpov13 = ninpov13 / ntotal13,
+         proplesshs13 = nlesshs13 / ntotal13,
+         propsingle13 = nsingle13 / ntotal13) %>% select(ntotal13, prophisp13, propblack13, propinpov13, proplesshs13, propsingle13)
+
+annandale12 <- annandale12 %>% summarize(ntotal12 = sum(B03003_001E),
+                                         nhispanic12 = sum(B03003_003E),
+                                         nblack12 = sum(B02001_003E),
+                                         ninpov12 = sum(B17020_002E),
+                                         nlesshs12 = sum(B15003_002E + B15003_003E +  B15003_004E + B15003_005E + B15003_006E + B15003_007E + B15003_008E + B15003_009E +
+                                                           B15003_010E + B15003_011E + B15003_012E + B15003_013E + B15003_014E + B15003_015E),
+                                         nsingle12 = sum(B09005_004E + B09005_005E)) %>%
+  mutate(prophisp12 = nhispanic12 / ntotal12,
+         propblack12 = nblack12 / ntotal12,
+         propinpov12 = ninpov12 / ntotal12,
+         proplesshs12 = nlesshs12 / ntotal12,
+         propsingle12 = nsingle12 / ntotal12) %>% select(ntotal12, prophisp12, propblack12, propinpov12, proplesshs12, propsingle12)
+
+annandale <- cbind(annandale12, annandale13, annandale14, annandale15)
+annandale <- annandale %>% st_set_geometry(NULL)
+
+# Pivot
+annandale_lng <- annandale %>% pivot_longer(cols = 1:24, values_to = "prop")
+annandale_lng <- annandale_lng %>% arrange(name)
+annandale_lng <- annandale_lng %>% mutate(year = str_extract_all(name, "[0-9]{2}", simplify = TRUE),
+                                      name = str_extract_all(name, "[a-zA-Z]+", simplify = TRUE))
+annandale_lng$name <- as.factor(annandale_lng$name)
+annandale_lng$name <- factor(annandale_lng$name, levels = c("ntotal", "propblack", "prophisp", "propinpov", "proplesshs", "propsingle"),
+                           labels = c("Total population", "Proportion Black", "Proportion Hispanic", "Proportion in poverty", "Proportion <HS education",
+                                      "Proportion single parent"))
+
+# Plot
+ggplot(annandale_lng[annandale_lng$name != "Total population", ], aes(y = prop, x = year)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~name) +
+  theme_ipsum_tw() +
+  labs(title = "Annandale Select Population Characteristics, 2012-2015", x = "Year", y = "Proportion",
+       caption = "Source: American Community Survey.")
+
