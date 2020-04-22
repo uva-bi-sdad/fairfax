5521e185a8156523256087de47a6440cf827209b
Teja Pristavec <tp2sk@virginia.edu>
2020-04-22 14:03:57 -0400

    Started autocorrelation test

--- src/gentri/04_model.R
+++ src/gentri/04_model.R
@@ -30,7 +30,7 @@ rundata$type1218 <- relevel(rundata$type1218, ref = "Not vulnerable")
 # Output includes some iteration history and includes the final log-likelihood.
 # This value multiplied by two is then seen in the model summary as the Residual Deviance 
 # and it can be used in comparisons of nested models.
-testmodel <- multinom(type1218 ~ chg1218_tct_popdens + chg1218_tct_housdens + chg1218_tct_renters, data = rundata)
+testmodel <- multinom(type1218 ~ chg1218_tct_popdens, data = rundata@data)
 summary(testmodel)
 
 # Display as risk ratios (extract coefficients from the model and exponentiate)
@@ -49,10 +49,22 @@ p
 # Calculate predicted probabilities
 head(pp <- fitted(testmodel))
 
-
 # Compute Moran’s I statistic
 # To quantify the presence of spatial autocorrelation in the model residuals
 # Conduct a permutation test to assess its significance. 
 # The permutation test has the null hypothesis of no spatial autocorrelation and an alternative 
 # hypothesis of positive spatial autocorrelation.
+Wnb <- poly2nb(rundata, row.names = rundata@data$GEOID) # turn data into a neighborhood (nb) object
+Wlist <- nb2listw(Wnb, style = "B") # turn data into a listw object, the required form of binary spatial adjacency information (based on border sharing) used by the moran.mc function
+moran.mc(x = residuals(testmodel), listw = Wlist, nsim = 1000) # run spatial autocorrelation test
 
+length(resid(testmodel)) 
+length(Wlist)
+
+### But it works for a logistic
+rundata$type1218 <- fct_collapse(rundata$type1218,
+             notgentrified = c("Not vulnerable", "Vulnerable, did not gentrify"),
+             gentrified = "Vulnerable, gentrified")
+             
+testmodel <- glm(type1218 ~ chg1218_tct_popdens,
+    data = rundata@data, family = binomial(link = "logit"))
